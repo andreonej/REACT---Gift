ffb9581e56060eca96af5b6d877f29f4f2d1fda9
Jose <jm.andreone@gmail.com>
2025-09-27 20:44:08 -0300

    FEAT : Generamos cache para las llamadas de la API

--- src/GifsApp.tsx
+++ src/GifsApp.tsx
@@ -1,34 +1,13 @@
-import { useState } from 'react';
-
 import { GifList } from './gifs/components/GifList';
 import { PreviousSearches } from './gifs/components/PreviousSearches';
 
 import { CustomHeader } from './shared/components/CustomHeader';
 import { SearchBar } from './shared/components/SearchBar';
 
-import { getGifsByQuery } from './gifs/actions/get-gifs-by-query.action';
-import type { Gif } from './gifs/interfaces/gif.interface';
+import { useGifs } from './gifs/hooks/useGifs';
 
 export const GifsApp = () => {
-  const [gifs, setGifs] = useState<Gif[]>([]);
-  const [previousTerms, setPreviousTerms] = useState<string[]>([]);
-
-  const handleTermClicked = (term: string) => {
-    console.log({ term });
-  };
-
-  const handleSearch = async (query: string = '') => {
-
-    query = query.trim().toLowerCase(); // Quita los espacios vacios al inicio y al final y convierte a minúsculas
-    if (query.length === 0) return; // Sale si la consulta esta vacía
-
-    if (previousTerms.includes(query)) return; // Si ya ya lo busque anteriormente, no hace nada
-
-    setPreviousTerms([query, ...previousTerms].splice(0, 8)); // Agrega la consulta al inicio y limita a 8 términos
-
-    const gifs = await getGifsByQuery(query);
-    setGifs(gifs);
-  };
+  const { handleSearch, previousTerms, handleTermClicked, gifs } = useGifs();
 
   return (
     <>
